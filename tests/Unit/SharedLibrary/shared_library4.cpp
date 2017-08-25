41a3c6506d5a9645cee869008f16ee77724b3a50
Pekka Jääskeläinen <pekka.jaaskelainen@parmance.com>
2017-08-25 14:21:30 +0200

    Pass the old LD_LIBRARY_PATH too.

--- tests/Unit/SharedLibrary/shared_library4.cpp
+++ tests/Unit/SharedLibrary/shared_library4.cpp
@@ -1,6 +1,6 @@
 // XFAIL: Linux
 // RUN: %hc -fPIC -Wl,-Bsymbolic -shared -DSHARED_LIBRARY %s -o %T/libtest4.so
-// RUN: %hc %s -L%T -ltest4 -o %t.out && LD_LIBRARY_PATH=%T %t.out
+// RUN: %hc %s -L%T -ltest4 -o %t.out && LD_LIBRARY_PATH=$LD_LIBRARY_PATH:%T %t.out
 
 // kernels built as one single shared libary
 // linked dynamically with the main program which contains
